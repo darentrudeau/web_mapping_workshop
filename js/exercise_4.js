9fd2ef8461de3fbdc3fdaec1ac4208c5635c61a9
darentrudeau <darentrudeau@users.noreply.github.com>
2016-05-02 16:04:39 -0400

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -17,7 +17,7 @@ map.setView([39, -96], 4);
 
 // Great, now we have a basic web map!
 
-var dataFileToAdd = 'data/restaurants.goejson';
+var dataFileToAdd = 'data/restaurants.geojson';
 
 var featureLayer = L.mapbox.featureLayer();
 
